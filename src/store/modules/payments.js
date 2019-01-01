16a571707e038598c759398dd9434092eaa5dad2
moontasirdev <moontasir.dev@outlook.com>
2018-12-31 21:38:40 -0500

    remove stub data

--- src/store/modules/payments.js
+++ src/store/modules/payments.js
@@ -10,27 +10,6 @@ const actions = {
     async fetchPayments({
         commit
     }) {
-        const tmp = [{
-            "id": "sdfs2w323",
-            "date": "01/01/2018",
-            "type": "ACH",
-            "user-type": "primary",
-            "amount": "120.01",
-            "bank": {
-                "name": "BOA",
-                "accountNumber": "12123"
-            }
-        }, {
-            "id": "sfsd2w323",
-            "date": "02/02/2018",
-            "type": "ACH",
-            "user-type": "primary",
-            "amount": "200.21",
-            "bank": {
-                "name": "Chase",
-                "accountNumber": "112233"
-            }
-        }];
         const response = await paymentApi.getPayments();
         console.log('calling api');
         console.log(response)
